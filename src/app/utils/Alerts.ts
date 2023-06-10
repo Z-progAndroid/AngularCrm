911bbc8c87e4c5589f8f78dd3c2b11c27a4683e9
Pablo Marco <pmarcomunoz@gmail.com>
2023-06-10 12:33:59 +0200

    T04- instalacion sweetalert y clase para mostrar alertas

---
+++ src/app/utils/Alerts.ts
@@ -0,0 +1,56 @@
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class Alerts {
+    static router: Router;
+
+    constructor(private router: Router) {
+        Alerts.router = router;
+    }
+    static error(titulo: string, mensaje: string): any {
+        return Swal.fire({
+            icon: 'error',
+            title: titulo,
+            text: mensaje,
+        })
+    }
+    static errorConRedirect(titulo: string, mensaje: string, url: string): any {
+        return Swal.fire({
+            icon: 'error',
+            title: titulo,
+            text: mensaje,
+        }).then((result) => {
+            this.router.navigate([url]);
+        })
+    }
+    static success(titulo: string, mensaje: string): any {
+        return Swal.fire({
+            icon: 'success',
+            title: titulo,
+            text: mensaje,
+        })
+    }
+
+    static warning(titulo: string, mensaje: string, confirmButtonText: string): any {
+        return Swal.fire({
+            title: titulo,
+            text: mensaje,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: confirmButtonText
+        })
+    }
+    static info(titulo: string, mensaje: string): any {
+        return Swal.fire({
+            icon: 'info',
+            title: titulo,
+            text: mensaje,
+        })
+    }
+}
