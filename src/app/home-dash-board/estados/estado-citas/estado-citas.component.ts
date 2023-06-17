4316a83eb5a4d2a12834916b06e38bb17df9788d
Pablo Marco <pmarcomunoz@gmail.com>
2023-06-17 14:21:05 +0200

    T04 estadocitas  crud

--- src/app/home-dash-board/estados/estado-citas/estado-citas.component.ts
+++ src/app/home-dash-board/estados/estado-citas/estado-citas.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { EstadoCitas } from 'src/app/models/estado-citas';
+import { Mensaje } from 'src/app/models/mensaje';
 import { EstadoCitasService } from 'src/app/services/estado-citas.service';
+import { Alerts } from 'src/app/utils/Alerts';
+import { Utils } from 'src/app/utils/Utils';
 
 @Component({
   selector: 'app-estado-citas',
@@ -9,55 +12,88 @@ import { EstadoCitasService } from 'src/app/services/estado-citas.service';
   styleUrls: ['./estado-citas.component.scss']
 })
 export class EstadoCitasComponent implements OnInit {
-  estadoCitasForm: FormGroup
-  estadosCita: EstadoCitas[]
+  estadoCitasForm: FormGroup;
+  estadosCita: EstadoCitas[];
+
   constructor(
     private fb: FormBuilder,
     private estadoCitasService: EstadoCitasService
   ) { }
-  ngOnInit(): void {
-    this.estadoCitasForm = this.fb.group({
-      idEstadoCita: ['', Validators.required],
-      estadoCita: ['', Validators.required],
-    })
-    this.estadoCitasService.findAll().subscribe(data => {
-      this.estadosCita = data
-    }), error => console.log(error);
-  }
-  submit() {
-    let estadoCita: EstadoCitas = new EstadoCitas();
-    estadoCita = Object.assign({}, this.estadoCitasForm.value);
-    estadoCita.fechaCreacion = new Date();
-    estadoCita.fechaModificacion = new Date();
-    estadoCita.modificado = 'admin';
-    this.estadoCitasService.save(estadoCita).subscribe(data => {
-      this.estadoCitasForm.reset();
-      this.ngOnInit();
-    }), error => console.log(error);
 
+  ngOnInit(): void {
+    this.crearFormulario();
+    this.cargarEstadosCita();
   }
+
+  submit() {
+    this.estadoCitasService.save(this.estadoCita).subscribe(
+      (data: any) => {
+        Alerts.success('Éxito', 'Estado guardado correctamente');
+        this.estadoCitasForm.reset();
+        this.cargarEstadosCita();
+      },
+      (error: any) => Alerts.error('Error', 'No se pudo guardar el estado', error)
+    );
+  }
+
   ver(id: number) {
     this.estadoCitasService.findById(id).subscribe(
       (estadoCita: EstadoCitas) => {
         this.estadoCitasForm.patchValue(estadoCita);
         this.estadoCitasForm.disable();
-      }), error => console.log(error);
+      },
+      (error: any) => Alerts.error('Error', 'No se pudo obtener el estado', error)
+    );
   }
+
   editar(id: number) {
     this.estadoCitasService.findById(id).subscribe(
       (estadoCita: EstadoCitas) => {
         this.estadoCitasForm.patchValue(estadoCita);
         this.estadoCitasForm.enable();
-      }), error => console.log(error);
+      },
+      (error: any) => Alerts.error('Error', 'No se pudo obtener el estado', error)
+    );
   }
+
   eliminar(id: number) {
-    this.estadoCitasService.delete(id).subscribe(
-      (mensaje: any) => {
-        console.log(mensaje);
-        this.ngOnInit();
-      }), error => console.log(error);
+    Alerts.warning('Advertencia', '¿Está seguro que desea eliminar el estado?', 'Eliminar').then((result: any) => {
+      if (!result.isConfirmed) {
+        return;
+      }
+      this.estadoCitasService.delete(id).subscribe(
+        (mensaje: Mensaje) => {
+          Alerts.success('Éxito', 'Estado eliminado correctamente').then(() => this.cargarEstadosCita());
+        },
+        (error: any) => Alerts.error('Error', 'Error al eliminar el estado', error)
+      );
+    });
   }
-  deshabilitar(id: number): boolean {
-    return id === 0;
-  }  
+
+  crearFormulario() {
+    this.estadoCitasForm = this.fb.group({
+      idEstadoCita: ['', Validators.required],
+      estadoCita: ['', Validators.required],
+      fechaCreacion: ['']
+    });
+  }
+
+  cargarEstadosCita() {
+    this.estadosCita = [];
+    this.estadoCitasService.findAll().subscribe(
+      (data: EstadoCitas[]) => {
+        this.estadosCita = data;
+      },
+      (error: any) => Alerts.error('Error', 'Error al cargar los estados', error)
+    );
+  }
+  private get estadoCita(): EstadoCitas {
+    let estadoCita: EstadoCitas = Object.assign({}, this.estadoCitasForm.value);
+    estadoCita.fechaModificacion = new Date();
+    estadoCita.modificado = 'admin';
+    Utils.isNullOrUndefined(estadoCita.fechaCreacion)
+      ? estadoCita.fechaCreacion = new Date()
+      : estadoCita.fechaCreacion = estadoCita.fechaCreacion;
+    return estadoCita;
+  }
 }
