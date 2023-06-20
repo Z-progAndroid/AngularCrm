6ab5212162da2cd063142fe8e6d006290beb5fcd
Pablo Marco <pmarcomunoz@gmail.com>
2023-06-20 20:59:36 +0200

    T05 crud tipo seguimiento

--- src/app/home-dash-board/tipos/tipo-seguimientos/tipo-seguimientos.component.ts
+++ src/app/home-dash-board/tipos/tipo-seguimientos/tipo-seguimientos.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TipoSeguimientos } from 'src/app/models/tipo-seguimientos';
 import { TipoSeguimientosService } from 'src/app/services/tipo-seguimientos.service';
+import { Alerts } from 'src/app/utils/Alerts';
+import { Utils } from 'src/app/utils/Utils';
 
 @Component({
   selector: 'app-tipo-seguimientos',
@@ -10,58 +12,82 @@ import { TipoSeguimientosService } from 'src/app/services/tipo-seguimientos.serv
 })
 export class TipoSeguimientosComponent {
   tipoSegiminetosForm: FormGroup
-  tipoSeguimiento: TipoSeguimientos []
+  tiposSeguimientos: TipoSeguimientos[]
   constructor(
     private fb: FormBuilder,
     private tipoSeguimientoService: TipoSeguimientosService
   ) { }
   ngOnInit(): void {
-    this.tipoSegiminetosForm = this.fb.group({
-      idTipoSeguimiento: ['', Validators.required],
-      tipoSeguimiento: ['', Validators.required]
-    });
-    this.tipoSeguimientoService.findAll().subscribe(tipoSeguimiento => this.tipoSeguimiento = tipoSeguimiento)
-      , error => console.log(error);
+    this.crearFormulario();
+    this.cargarTipoSeguimientos();
   }
   submit() {
-    let tipoSeguimiento: TipoSeguimientos=new TipoSeguimientos();
-    tipoSeguimiento.idTipoSeguimiento = this.tipoSegiminetosForm.get('idTipoSeguimiento').value;
-    tipoSeguimiento.tipoSeguimiento = this.tipoSegiminetosForm.get('tipoSeguimiento').value;
-    tipoSeguimiento.fechaCreacion = new Date();
-    tipoSeguimiento.fechaModificacion = new Date();
-    tipoSeguimiento.modificado = 'admin';
-    this.tipoSeguimientoService.save(tipoSeguimiento).subscribe((tipoSeguimiento: TipoSeguimientos) => {
-        this.tipoSeguimientoService.findAll().subscribe(
-          tipoSeguimiento => this.tipoSeguimiento = tipoSeguimiento
-        ), error => console.log(error);
-      }, error => console.log(error));
-
+    Alerts.warning('Advertencia', '¿Está seguro de guardar el tipo de seguimiento ?', 'Si,guardar')
+      .then((result) => {
+        if (!result.isConfirmed) {
+          Alerts.info('Información', 'Operación cancelada por el usuario');
+          this.tipoSegiminetosForm.reset();
+          return;
+        }
+        this.tipoSeguimientoService.save(this.tipoSeguimiento).subscribe((tipoSeguimiento: TipoSeguimientos) => {
+          Alerts.success('Exito', 'Tipo de seguimiento guardado correctamente');
+          this.tipoSegiminetosForm.reset();
+          this.cargarTipoSeguimientos();
+        }, error => Alerts.error('Error', 'Error al guardar el tipo de seguimiento', error));
+      });
   }
   editar(id: number) {
     this.tipoSeguimientoService.findById(id).subscribe(
       (tipoSeguimiento: TipoSeguimientos) => {
         this.tipoSegiminetosForm.patchValue(tipoSeguimiento);
         this.tipoSegiminetosForm.enable();
-      }), error => console.log(error);
+      }, error => Alerts.error('Error', 'Error al cargar el tipo de seguimiento', error));
 
   }
   eliminar(id: number) {
-    this.tipoSeguimientoService.delete(id).subscribe(
-      (mensaje: any) => {
-        this.tipoSeguimientoService.findAll().subscribe(
-          tipoSeguimiento => this.tipoSeguimiento = tipoSeguimiento
-        ), error => console.log(error);
-      }), error => console.log(error);
+    Alerts.warning('Advertencia', '¿Está seguro de eliminar el tipo de seguimiento ?', 'Si,guardar')
+      .then((result) => {
+        if (!result.isConfirmed) {
+          Alerts.info('Información', 'Operación cancelada por el usuario');
+          this.tipoSegiminetosForm.reset();
+          return;
+        }
+        this.tipoSeguimientoService.delete(id).subscribe(
+          (mensaje: any) => {
+            Alerts.success('Exito', 'Tipo de seguimiento eliminado correctamente');
+            this.tipoSegiminetosForm.reset();
+            this.cargarTipoSeguimientos();
+          }, error => Alerts.error('Error', 'Error al eliminar el tipo de seguimiento', error));
+      });
   }
   ver(id: number) {
     this.tipoSeguimientoService.findById(id).subscribe(
       (tipoSeguimiento: TipoSeguimientos) => {
         this.tipoSegiminetosForm.patchValue(tipoSeguimiento);
         this.tipoSegiminetosForm.disable();
-      }
-    ), error => console.log(error);
+      }, error => Alerts.error('Error', 'Error al cargar el tipo de seguimiento', error));
   }
-  deshabilitar(id: number): boolean {
-    return id === 0;
+  cargarTipoSeguimientos() {
+    this.tipoSeguimientoService.findAll().subscribe(tipoSeguimiento => {
+      this.tiposSeguimientos = tipoSeguimiento
+    }, error => Alerts.error('Error', 'Error al cargar los tipos de seguimientos', error));
+  }
+  crearFormulario() {
+    this.tipoSegiminetosForm = this.fb.group({
+      idTipoSeguimiento: ['', Validators.required],
+      tipoSeguimiento: ['', Validators.required],
+      fechaCreacion: [''],
+    });
+  }
+  private get tipoSeguimiento(): TipoSeguimientos {
+    let tipoSeguimiento: TipoSeguimientos = new TipoSeguimientos();
+    tipoSeguimiento.idTipoSeguimiento = this.tipoSegiminetosForm.get('idTipoSeguimiento').value;
+    tipoSeguimiento.tipoSeguimiento = this.tipoSegiminetosForm.get('tipoSeguimiento').value;
+    tipoSeguimiento.fechaCreacion = Utils.isNullOrUndefined(this.tipoSegiminetosForm.get('fechaCreacion').value)
+      ? new Date()
+      : this.tipoSegiminetosForm.get('fechaCreacion').value;
+    tipoSeguimiento.fechaModificacion = new Date();
+    tipoSeguimiento.modificado = 'admin';
+    return tipoSeguimiento;
   }
 }
