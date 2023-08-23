1ab3c18b9082a6ca8c409091d131cbc72baa692f
Pablo Marco <pmarcomunoz@gmail.com>
2023-08-24 01:08:35 +0200

    T13-DASHBORAD GRAFICOS

---
+++ src/app/models/grafico.ts
@@ -0,0 +1,7 @@
+export class Grafico {
+    contratos: { [key: string]: number };
+    inmuebles: { [key: string]: number };
+    citas: { [key: string]: number };
+    tareas: { [key: string]: number };
+    visitas: { [key: string]: number };
+}
