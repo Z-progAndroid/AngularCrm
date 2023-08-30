e2ddb47748e3cf52d8d2428d1e0c1f08b3a2c74c
Pablo Marco <pmarcomunoz@gmail.com>
2023-08-31 01:35:04 +0200

    T15- TITULOS CITAS

--- src/app/home-dash-board/cita/cita.component.ts
+++ src/app/home-dash-board/cita/cita.component.ts
@@ -14,7 +14,10 @@ import { TipoCitaService } from 'src/app/services/tipo-cita.service';
 import { TipoCita } from 'src/app/models/tipo-cita';
 import { EstadoCitasService } from 'src/app/services/estado-citas.service';
 import { EstadoCitas } from 'src/app/models/estado-citas';
-import { TableColumn } from 'src/app/interfaces/table-column';
+import Tooltip from 'tooltip.js';
+const TITULO = " TITULO: ";
+const TIPO_CITA = " TIPO CITA: ";
+const ESTADO_CITA = " ESTADO CITA: ";
 @Component({
   selector: 'app-cita',
   templateUrl: './cita.component.html',
@@ -33,89 +36,7 @@ export class CitaComponent extends BaseComponent implements OnInit {
   ) { super(); }
   ngOnInit(): void {
     this.cargarCitas();
-    this.crearLeyenda();
-    this.crearLeyendaEstados();
   }
-  crearLeyenda() {
-    this.tipoCitaService.findAll().subscribe((tipoCitas: TipoCita[]) => {
-      tipoCitas.forEach(element => {
-        if (element.tipoCita === 'VISTA PROPIEDAD') {
-          this.legend.push({ nombre: 'VISTA PROPIEDAD', color: 'background-color:#66b2b2;' });
-          return;
-        }
-        if (element.tipoCita === 'EVALUACIÓN DE PROPIEDAD') {
-          this.legend.push({ nombre: 'EVALUACIÓN DE PROPIEDAD', color: 'background-color:#6699cc;' });
-          return;
-        }
-        if (element.tipoCita === 'FIRMA DE CONTRATOS') {
-          this.legend.push({ nombre: 'FIRMA DE CONTRATOS', color: 'background-color:#6699ff;' });
-          return;
-        }
-        if (element.tipoCita === 'NEGOCIACIÓN') {
-          this.legend.push({ nombre: 'NEGOCIACIÓN', color: 'background-color:#b3d9ff;' });
-          return;
-        }
-        if (element.tipoCita === 'ASESORAMIENTO') {
-          this.legend.push({ nombre: 'ASESORAMIENTO', color: 'background-color:#99ccff;' });
-          return;
-        }
-        this.legend.push({ nombre: 'SIN CLASIFICAR', color: 'background-color:#E3E3E3;' });
-      });
-    }, error => {
-      Alerts.error('Error', 'Error al cargar los tipos de citas', error);
-    });
-  }
-  crearLeyendaEstados() {
-    /*   'POR DEFECTO #CCCCCC';
-   'PENDIENTE #FFFF00';
-   'CONFIRMADA #00FF00';
-   'CANCELADA #FF0000';
-   'REALIZADA #0000FF';
-   'REAGENDADA #FFA500';
-   'ELIMINADO #333333';
-   'ACTIVA #00FF99';
-     */
-    this.estadoCitaService.findAll().subscribe((estadoCitas: EstadoCitas[]) => {
-      estadoCitas.forEach(element => {
-        if (element.estadoCita === 'POR DEFECTO') {
-          this.legendEstado.push({ nombre: 'POR DEFECTO', color: 'background-color:##CCCCCC;' });
-          return;
-        }
-        if (element.estadoCita === 'PENDIENTE') {
-          this.legendEstado.push({ nombre: 'PENDIENTE', color: 'background-color:#FFFF00;' });
-          return;
-        }
-        if (element.estadoCita === 'CONFIRMADA') {
-          this.legendEstado.push({ nombre: 'CONFIRMADA', color: 'background-color:#00CC00;' });
-          return;
-        }
-        if (element.estadoCita === 'CANCELADA') {
-          this.legendEstado.push({ nombre: 'CANCELADA', color: 'background-color:#FF0000;' });
-          return;
-        }
-        if (element.estadoCita === 'REALIZADA') {
-          this.legendEstado.push({ nombre: 'REALIZADA', color: 'background-color:#0000FF;' });
-          return;
-        }
-        if (element.estadoCita === 'REAGENDADA') {
-          this.legendEstado.push({ nombre: 'REAGENDADA', color: 'background-color:#FFA500;' });
-          return;
-        }
-        if (element.estadoCita === 'ELIMINADO') {
-          this.legendEstado.push({ nombre: 'ELIMINADO', color: 'background-color:#333333;' });
-          return;
-        }
-        if (element.estadoCita === 'ACTIVA') {
-          this.legendEstado.push({ nombre: 'ACTIVA', color: 'background-color:#33FF33;' });
-          return;
-        }
-      });
-    }, error => {
-      Alerts.error('Error', 'Error al cargar los tipos de citas', error);
-    });
-  }
-
-
   calendarGridOptions: CalendarOptions = {
     headerToolbar: {
       left: 'title',
@@ -128,6 +49,11 @@ export class CitaComponent extends BaseComponent implements OnInit {
     eventClick: this.handleEventClick.bind(this),
     dateClick: this.handleDateClick.bind(this),
     locale: esLocale,
+    eventDidMount: function (info) {
+      info.event._def.extendedProps.estado
+      let title = TITULO + info?.event?._def?.title + TIPO_CITA + info?.event?._def?.extendedProps?.tipo + ESTADO_CITA + info?.event?._def?.extendedProps?.estado;
+      info.el.setAttribute("title", title);
+    },
     events: []
   };
 
@@ -158,6 +84,10 @@ export class CitaComponent extends BaseComponent implements OnInit {
         end: new Date(cita.fechaFin[0], cita.fechaFin[1] - 1, cita.fechaFin[2], cita.fechaFin[3], cita.fechaFin[4], cita.fechaFin[5]).toISOString(),
         backgroundColor: this.getTipoCitaColor(cita.idTipoCita),
         borderColor: this.getEstadoCitaColor(cita.idEstadoCita),
+        extendedProps: {
+          estado: cita.estadoCita,
+          tipo: cita.tipoCita,
+        },
       }));
       this.calendarGridOptions.events = citasFormateadas;
       this.calendarListOptions.events = citasFormateadas;
@@ -165,7 +95,7 @@ export class CitaComponent extends BaseComponent implements OnInit {
   }
 
   getTipoCitaColor(tipoCita: number): string {
-    const tiposCitaColores = {1: '#66b2b2',2: '#6699cc',3: '#6699ff',4: '#b3d9ff',5: '#99ccff',};
+    const tiposCitaColores = { 1: '#66b2b2', 2: '#6699cc', 3: '#6699ff', 4: '#b3d9ff', 5: '#99ccff', };
     return tiposCitaColores[tipoCita] || 'gray';
   }
   getEstadoCitaColor(tipoCita: number): string {
